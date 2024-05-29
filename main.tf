b377781b37b311db2260b39ec2093b312179fefb
Azhar <azharshaikh33@gmail.com>
2024-05-29 10:37:40 +0400

    Auto Push

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
 module "alb-public" {
     source = "./vendor/modules/alb"
+    ALB_NAME = "roboshop-public-alb"
+    ENV = var.ENV
+    INTERNAL = false
    
 
 }
 
 module "alb-private" {
     source = "./vendor/modules/alb"
+    ALB_NAME = "roboshop-private-alb"
+    ENV = var.ENV
+    INTERNAL = true
    
 
 }
